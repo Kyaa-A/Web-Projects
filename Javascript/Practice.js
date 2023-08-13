1001cb121ac8bcf9ba071cd66c8e958c90ab5414
Kyaa-A <asnaripacalna@gmail.com>
2023-08-13 15:57:27 +0800

    init

---
+++ Javascript/Practice.js
@@ -0,0 +1,44 @@
+// Tasks
+
+
+/* 5. Ask for confirmation using if and else */
+
+// let isConfirm = confirm("Is this okay?");
+// if(isConfirm){
+//     alert("Confirmed");
+// }else{
+//     alert("Cancelled")
+// }
+
+
+/* 4. A simple page asking a name and outputs it. */
+
+// let askName = "";
+// let name = prompt("What is your name?", askName);
+// alert(`Your name is ${name}`)
+
+
+/* 3. In JavaScript, there are 3 types of quotes.
+
+Double quotes: "Hello".
+Single quotes: 'Hello'.
+Backticks: `Hello`. */
+
+// let name ="Asnari";
+// let age = 19;
+// alert(`Hi ${name} ur age is ${age}`)
+
+
+/* 2. Giving the right name */
+
+// let ourPlanet = "Earth";
+// let currentVisitor = "John";
+
+
+/* 1. Working with Variables */
+
+// let admin;
+// let name = "John";
+// admin = name;
+// alert(admin);
+
